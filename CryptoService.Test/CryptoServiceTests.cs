d39f72543b64851858967d319def064a91ef5d7c
Jonathan Withers <jonathan_withers@hotmail.com>
2017-11-18 22:46:10 +0000

    Initial add

---
+++ CryptoService.Test/CryptoServiceTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CryptoService.Test
+{
+	/// <summary>
+	/// https://stackoverflow.com/questions/10168240/encrypting-decrypting-a-string-in-c-sharp
+	/// </summary>
+	[TestClass]
+	public class CryptoServiceTests
+	{
+		[TestMethod]
+		public void CanEncryptAndDecryptSampleStringCorrectly()
+		{
+			// Arrange
+			var plainText = "This is my sample plain text message";
+			var passPhrase = "supersecretpassword";
+
+			// Act
+			var cipherText = StringCipher.Encrypt(plainText, passPhrase);
+			var decryptedText = StringCipher.Decrypt(cipherText, passPhrase);
+
+			// Assert
+			Assert.AreEqual(plainText, decryptedText);
+		}
+
+		[TestMethod]
+		public void EncryptingTheSamePlainTextWithTheSamePasswordMultipleTimesProducesDifferentCipherText()
+		{
+			// Arrange
+			var plainText = "This is my sample plain text message";
+			var passPhrase = "supersecretpassword";
+
+			// Act
+			var cipherText1 = StringCipher.Encrypt(plainText, passPhrase);
+			var cipherText2 = StringCipher.Encrypt(plainText, passPhrase);
+
+			// Assert
+			Assert.AreNotEqual(cipherText1, cipherText2);
+		}
+
+		[TestMethod]
+		public void EncryptingTheSamePlainTextWithTheSamePasswordMultipleTimesProducesDifferentCipherTextButBothCanBeDecryptedCorrectly()
+		{
+			// Arrange
+			var plainText = "This is my sample plain text message";
+			var passPhrase = "supersecretpassword";
+
+			// Act
+			var cipherText1 = StringCipher.Encrypt(plainText, passPhrase);
+			var cipherText2 = StringCipher.Encrypt(plainText, passPhrase);
+			var decryptedText1 = StringCipher.Decrypt(cipherText1, passPhrase);
+			var decryptedText2 = StringCipher.Decrypt(cipherText2, passPhrase);
+
+			// Assert
+			Assert.AreEqual(decryptedText1, plainText);
+			Assert.AreEqual(decryptedText1, decryptedText2);
+		}
+	}
+}
---
+++ CryptoService/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace CryptoService
+{
+	class Program
+	{
+		[STAThread]
+		static void Main(string[] args)
+		{
+			try
+			{
+				Console.WriteLine("Please select a mode:\n(E)ncrypt\n(D)ecrypt");
+				var keyInfo = Console.ReadKey(true);
+
+				if (keyInfo.Key == ConsoleKey.E) Encrypt();
+				else if (keyInfo.Key == ConsoleKey.D) Decrypt();
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine($"\nError: {ex.Message}\n\nPress any key to exit...");
+			}
+			Console.ReadLine();
+		}
+
+		static void Encrypt()
+		{
+			Console.WriteLine("\nPlease enter a string to encrypt:");
+			string plaintext = Console.ReadLine();
+
+			Console.WriteLine("\nPlease enter a password to use:");
+			string password = Console.ReadLine();
+			
+			Console.WriteLine($"\nYour encrypted string is:\n{StringCipher.Encrypt(plaintext, password)}\n\nPress any key to exit...");
+		}
+
+		static void Decrypt()
+		{
+			Console.WriteLine("\nPlease enter the encrypted string:");
+			string encryptedstring = Console.ReadLine();
+
+			Console.WriteLine("\nPlease enter the password:");
+			string password = Console.ReadLine();
+
+			Console.WriteLine($"\nYour decrypted string is:\n{StringCipher.Decrypt(encryptedstring, password)}\n\nPress any key to exit...");
+		}
+	}
+}
